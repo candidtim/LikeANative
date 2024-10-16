5ee33f00344d1da96b84a7f1bec72fa814cd1a69
candidtim <timcandid@gmail.com>
2024-10-16 23:27:09 +0200

    lang option

--- Sources/LikeNativeCLI.swift
+++ Sources/LikeNativeCLI.swift
@@ -2,27 +2,28 @@ import ArgumentParser
 import Foundation
 import OpenAI
 
-let prompt = """
-    You are the grammar and spelling corrector. User wrote a text in French. User is not a native French speaker.
+let promptTemplate = """
+    You are the grammar and spelling corrector. User wrote a text in %@. User is not a native %@ speaker.
     Your task is to rephrase the text and make it sound like a native speaker would say it. The rules are:
     - Keep the same meaning of the original message. As much as possible preserve the users writing style.
     - Introduce as few changes as possible.
     - Only change the message when something is very different from how a native speaker would say it, or if there is an error.
     - Only output the result and nothing else. Do not add any other remarks.
-    - If the text is obviously mixed English and French (e.g., contains technical terms in English), keep the original English words.
+    - If the text is obviously mixed English and %@ (e.g., contains technical terms in English), keep the original English words.
     - Do not introduce or remove new lines.
     """
 
 @main
 @available(macOS 12, *)
 struct LikeNative: AsyncParsableCommand {
-    @Argument
-    var input: String
+    @Argument var input: String
+    @Option var lang: String = "French"
 
     mutating func run() async throws {
         let token = ProcessInfo.processInfo.environment["OPENAI_API_KEY"]!
         let openAI = OpenAI(apiToken: token)
 
+        let prompt = String(format: promptTemplate, self.lang, self.lang, self.lang)
         let query = ChatQuery(
             messages: [
                 .init(role: .system, content: prompt)!,
