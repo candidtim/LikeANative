6495bee68469d917749fc88bde2f6e1d1c2aa75b
candidtim <timcandid@gmail.com>
2024-10-17 20:26:26 +0200

    Clearer error handling. Added README.

---
+++ README.md
@@ -0,0 +1,89 @@
+# Like a native
+
+An AI-based (GPT) spelling and grammar corrector that transforms user input to sound
+like a native speaker, with minimal style alterations.
+
+This tool is ideally suited for scenarios where your understanding of a foreign
+language has surpassed the point requiring an automated translator, you desire
+to sound authentic, and yet, you still make those minor errors which are so
+obvious to any native speaker.
+
+## Status
+
+Stable. Not distributed. Installing from the source is straightforward and
+described below, but requires a little bit of a "hands-on" approach.
+
+## Pre-requisites
+
+Obtain an [OpenAI API key](https://platform.openai.com/account/api-keys).
+
+OpenAI API key should be available in the `OPENAI_API_KEY` env var. E.g., add
+this to your `~./zshrc` file:
+
+    export OPENAI_API_KEY="sk-..."
+
+The tool uses the "GPT-4o mini" model, which is the cheapest at the time of
+this writing. Be aware of the [prices](https://openai.com/api/pricing/) you
+incur by using the tool with your token.
+
+## Build
+
+    swift build
+
+## Usage
+
+The application is a command line tool. When run, it replaces the text in the
+clipboard with a corrected version.
+
+For instance, to execute from the command line directly from the source directory:
+
+    swift run LikeNativeCLI --lang LANG
+
+where `LANG` is a common name of any language GPT4 might know well. E.g.,
+"French", "German", etc.
+
+### Keyboard shortcut
+
+You can set up a keyboard shortcut to run the tool. A typical scenario is:
+write some text (in a chat or an email, etc.), select and copy it to the
+clipboard (`⌘-A`, `⌘-C`), use the keyboard shortcut to correct it, then paste
+the corrected text back in place of the previous selection (`⌘-V`). Neat!
+
+1. Create an Automator Service:
+
+   - Open Automator from Applications
+   - Choose "Quick Action" as the document type
+   - In the workflow area, change "Workflow receives current" to "no input"
+   - Search for "Run Shell Script" in the actions list and drag it into the
+     workflow area
+   - The shell command should pass the API key in an environment variable, and
+     then run the built binary. Keep the `&` at the end. See the example below.
+   - Save the workflow with a name like "Like a native".
+
+Example Automator command:
+
+    OPENAI_API_KEY="sk-..." /Users/me/like-a-native/.build/arm64-apple-macosx/debug/LikeNativeCLI --lang French &
+
+2. Assign a Keyboard Shortcut:
+
+   - Open System Settings > Keyboard > Keyboard Shortcuts
+   - Go to Services and find your saved Automator workflow under the "General"
+     section
+   - Assign a custom keyboard shortcut to it
+
+Naturally, you can set up multiple keyboard shortcuts, each for a different language.
+
+When you use the keyboard shortcut, a cog icon will appear in the system menu
+bar, and then disappear once the text is corrected.
+
+### Mistakes
+
+Beware that this tool uses the "GPT-4o mini" model and can make mistakes, such
+as providing inconsistent responses or refusing to correct the text. In this
+case, you will receive the tool's output back to the clipboard.
+
+## Implementation. Prompt engineering.
+
+Technically, you can adjust the GPT prompt to carry out any sort of edits or
+corrections to the text in the clipboard. Tweak the `promptTemplate` as
+required. Please feel free to submit your prompts via GitHub issues.
--- Sources/LikeNativeCLI.swift
+++ Sources/LikeNativeCLI.swift
@@ -3,42 +3,89 @@ import ArgumentParser
 import Foundation
 import OpenAI
 
-let promptTemplate = """
-    You are the grammar and spelling corrector. User wrote a text in %@. User is not a native %@ speaker.
-    Your task is to rephrase the text and make it sound like a native speaker would say it. The rules are:
-    - Keep the same meaning of the original message. As much as possible preserve the users writing style.
-    - Introduce as few changes as possible.
-    - Only change the message when something is very different from how a native speaker would say it, or if there is an error.
-    - Only output the result and nothing else. Do not add any other remarks.
-    - If the text is obviously mixed English and %@ (e.g., contains technical terms in English), keep the original English words.
-    - Do not introduce or remove new lines.
-    """
-
 @main
-@available(macOS 12, *)
+@available(macOS 12, *) // to properly parse the options with async run function
 struct LikeNative: AsyncParsableCommand {
     @Option var lang: String = "French"
 
     mutating func run() async throws {
         let pasteboard = NSPasteboard.general
         if let input = pasteboard.string(forType: .string) {
-            let token = ProcessInfo.processInfo.environment["OPENAI_API_KEY"]!
-            let openAI = OpenAI(apiToken: token)
-
-            let prompt = String(format: promptTemplate, self.lang, self.lang, self.lang)
-            let query = ChatQuery(
-                messages: [
-                    .init(role: .system, content: prompt)!,
-                    .init(role: .user, content: input)!,
-                ], model: .gpt4)
-            let result = try await openAI.chats(query: query)
-            let output = result.choices[0].message.content!.string!
+            let corrector = newCorrector(lang: self.lang)
+            let output = await corrector?.correct(text: input) ?? input
             pasteboard.clearContents()
             pasteboard.setString(output, forType: .string)
-            print(output)
-            print("\n--\nAbove output is now in the pasteboard")
-        } else {
-            print("Nothing in the pasteboard")
         }
+    }
+}
+
+func fail(msg: String) {
+    print("ERROR: \(msg)")
+    exit(1)
+}
+
+@available(macOS 10.15, *) // to use GPT4Corrector
+func newCorrector(lang: String) -> Corrector? {
+    // return DoNothingCorrector(lang: self.lang)
+    if let token = ProcessInfo.processInfo.environment["OPENAI_API_KEY"] {
+        return GPT4Corrector(apiToken: token, lang: lang)
+    } else {
+        fail(msg: "cannot find OPENAI_API_KEY env var")
+        return nil
+    }
+}
+
+class Corrector {
+    var lang: String
+
+    init(lang: String) {
+        self.lang = lang
+    }
+
+    func correct(text: String) async -> String {
+        fatalError("func 'correct' is not implemented")
+    }
+}
+
+@available(macOS 10.15, *)
+class GPT4Corrector: Corrector {
+    private static let promptTemplate = """
+        You are the grammar and spelling corrector. The user has written a text in %@, and they are not a native %@ speaker. Your task is to rephrase the text to sound like a native speaker would. The rules are:
+        * Maintain the original meaning of the message. Preserve the user's writing style as much as possible. If the input is an informal speach like often used in a chat - keep the informal style. If the input is obviously formal - keep the formal style. If the input is already correct - no need to change or make it better.
+        * Introduce as few changes as necessary.
+        * Only modify the message if something is significantly different from how a native speaker would express it, or if there's a mistake.
+        * Output only the corrected result, with no additional comments or remarks.
+        * If the text includes both English and %@ (e.g., technical terms in English), retain the original English words.
+        * Do not introduce or remove any line breaks.
+        """
+
+    private var apiToken: String
+
+    init(apiToken: String, lang: String) {
+        self.apiToken = apiToken
+        super.init(lang: lang)
+    }
+
+    override func correct(text: String) async -> String {
+        let openAI = OpenAI(apiToken: self.apiToken)
+        let prompt = String(format: GPT4Corrector.promptTemplate, self.lang, self.lang, self.lang)
+        let query = ChatQuery(
+            messages: [
+                .init(role: .system, content: prompt)!,
+                .init(role: .user, content: text)!,
+            ], model: .gpt4_o_mini)
+        do {
+            let result = try await openAI.chats(query: query) // requires macOS 10.15
+            // FIXME: better error handling
+            return result.choices[0].message.content?.string ?? text
+        } catch {
+            return text
+        }
+    }
+}
+
+class DoNothingCorrector: Corrector {
+    override func correct(text: String) async -> String {
+        return text
     }
 }
